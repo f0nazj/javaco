1e9a369e46179ee880da24f5a62dae7917f70200
SnowGodss <e424320741@gmail.com>
2026-05-07 01:01:34 +0800

    2025/5/6

--- src/animal/zoo/Text.java
+++ src/animal/zoo/Text.java
@@ -4,8 +4,11 @@ import tool.coding.ListNode;
 
 public class Text {
     public static void main(String[] args) {
-        ListNode current = head;
+        ListNode head = new ListNode(1);  // 先建立 head
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
 
+    ListNode current = head;
         while (current != null) {
             System.out.println(current.val);
             current = current.next;
